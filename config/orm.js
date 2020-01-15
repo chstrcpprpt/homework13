a568cb6daceccbd6d0fe73f3a9d436f27dd3f7e2
chstrcpprpt <cjhallen@gmail.com>
2020-01-16 07:56:56 +1100

    added selectAll, insertOne & updateOne in orm

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,45 @@
-// selectAll()
+const connection = require("./connection");
 
-// insertOne()
+const orm = {
+  // selectAll()
+  selectAll: (tableName, cb) => {
+    const queryString = 
+    `SELECT * FROM ${tableName};`;
+    connection.query(queryString, (err, data) => {
+      if (err) throw err;
+      cb(data);
+    });
+  },
 
-// updateOne()
+  // insertOne()
+  insertOne: (tableName, columnName1, value1, columnName2, value2, cb) => {
+    const queryString =
+    `INSERT INTO ${tableName}
+    SET
+    {
+      ${columnName1}: ${value1},
+      ${columnName2}: ${value2}
+    };`;
+    connection.query(queryString, (err, data) => {
+      if (err) throw err;
+      cb(data);
+    });
+  },
+
+  // updateOne()
+  updateOne: (tableName, columnName, value, cb) => {
+    const queryString =
+    `UPDATE ${tableName}
+    SET
+    {
+      ${columnName}: ${value}
+    };`;
+    connection.query(queryString, (err, date) => {
+      if (err) throw err;
+      cb(data);
+    });
+  } 
+
+};
+
+module.exports = orm;
