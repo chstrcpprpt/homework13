0a43b7db701f04bea3fc58fb50866ddaa2aaaf7b
chstrcpprpt <cjhallen@gmail.com>
2020-01-16 07:21:54 +1100

    Updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(50) NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
