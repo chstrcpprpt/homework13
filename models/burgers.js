0fa9ef4a6e9ae08f10cb754943baffa4f6ff5285
chstrcpprpt <cjhallen@gmail.com>
2020-01-16 08:11:01 +1100

    created burger object

---
+++ models/burgers.js
@@ -0,0 +1,25 @@
+const orm = require("../config/orm");
+
+const burger = {
+
+  all: (cb) => {
+    orm.selectAll("burgers", data => {
+      cb(data);
+    });
+  },
+
+  create: (burgerName, cb) => {
+    orm.insertOne("burgers", "burger_name", burgerName, "devoured", "false", data => {
+      cb(data);
+    });
+  },
+
+  update: (cb) => {
+    orm.updateOne("burgers", "devoured", "true", data => {
+      cd(data);
+    });
+  }
+
+};
+
+module.exports = burger;
