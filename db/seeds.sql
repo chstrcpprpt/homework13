76eac834418715310956d3f7f63be107b8fbf87b
chstrcpprpt <cjhallen@gmail.com>
2020-01-16 06:46:12 +1100

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royale with cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double beef double bacon", false);
