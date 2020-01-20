0d8f0e1a94c80b812e0075e8864f86ca7e66aa8d
chstrcpprpt <cjhallen@gmail.com>
2020-01-20 15:49:21 +1100

    added start of GET route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,10 +5,19 @@ const router = express.Router();
 // import model
 const burger = require("../models/burgers");
 
-// create routes
+// create routes - CHECK
 router.post("/api/burger", (req, res) => {
   burger.create(req.body.burger, res => {
     res.status(204).end();
   });
 });
+
+// GET
+router.get("/burger", (req, res) => {
+  burger.all((data) => {
+    res.json(data);
+  })
+});
+
+module.exports = router;
 
